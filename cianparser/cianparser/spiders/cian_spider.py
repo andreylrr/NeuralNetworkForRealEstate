1686ebb5793f62709a25b496dd78d4ab9836f300
Andrey Rumyantsev <andreylrr@gmail.com>
2019-10-21 19:18:44 +0300

    Spider was changed

--- cianparser/cianparser/spiders/cian_spider.py
+++ cianparser/cianparser/spiders/cian_spider.py
@@ -13,6 +13,19 @@ class CianSpider(scrapy.Spider):
         url = "https://" + url_split.netloc + link
         print(url)
         for page_number in range(2, MAXIMUM_PAGE_NUMBER):
+            l_url = map(lambda x: f"p={page_number}" if "p=" in x else x, url.split("&"))
+            s_url_page = "&".join(list(l_url))
+            yield scrapy.Request(s_url_page, callback=self.page_parser)
+
+    def page_parser(self, response):
+         l_link = response.xpath('//*[@id="frontend-serp"]/div/div[6]//h3/div/a/@href')
+         for link in l_link:
+             yield scrapy.Request(link, callback=self.item_parser)
+
+    def item_parser(self, response):
+
+         pass
+
 
 
 
