7cc419c2776a4df4872094a513b2ab187cbdc24b
Andrey Rumyantsev <andreylrr@gmail.com>
2019-10-22 19:37:45 +0300

    Maximum page number was changed and fix the problem with links

--- cianparser/cianparser/spiders/cian_spider.py
+++ cianparser/cianparser/spiders/cian_spider.py
@@ -1,22 +1,17 @@
 import scrapy
 import urllib.parse as ur
 
-MAXIMUM_PAGE_NUMBER = 100
+MAXIMUM_PAGE_NUMBER = 5
 
 class CianSpider(scrapy.Spider):
     name = "cianparser"
     start_urls = ['https://spb.cian.ru/kupit-ofis/',]
 
     def parse(self, response):
-        url_split = ur.urlsplit(self.start_urls[0])
-        link = response.xpath('//*[@id="frontend-serp"]/div/div[7]//li/a/@href').extract_first()
-        url = "https://" + url_split.netloc + link
-        print("==============  ",url)
+        url = response.xpath('//*[@id="frontend-serp"]/div/div[7]//li/a/@href').extract_first()
         for page_number in range(2, MAXIMUM_PAGE_NUMBER):
-            l_url = map(lambda x: f"p={page_number}" if "p=" in x else x, url.split("&"))
-            s_url_page = "&".join(list(l_url))
-            print ( "The link before join +++++++++++++", l_url)
-            print( "The first link ------- ", s_url_page)
+            m_url = map(lambda x: f"p={page_number}" if "p=" in x else x, url.split("&"))
+            s_url_page = "&".join(list(m_url))
             yield scrapy.Request(s_url_page, callback=self.page_parser)
 
     def page_parser(self, response):
